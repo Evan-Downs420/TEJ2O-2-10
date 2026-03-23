5561b2ea9ed7c7b87ab09f097e665ce6f61dd9b4
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-03-23 13:21:03 -0400

    wads

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Evan
- * Created on: Feb 2026
- * This program shows the brightness level of light
+*
+* Created by: Evan
+* Created on: Feb 2026
+* This program shows the brightness level of light
 */
+
 
 // variables
 let neopixelStrip: neopixel.Strip = null
 let lightAmount: number = 0
 
-// clean 
+
+// clean
 basic.clearScreen()
-neopixelStrip = neopixel.create(DigitalPin.P16,4,NeoPixelMode.RGB)
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
@@ -19,23 +21,29 @@ neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
+
 lightAmount = input.lightLevel()
+
 
 if (lightAmount <= 51) {
     neopixelStrip.clear()
 }
 
+
 if (lightAmount > 52) {
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
 }
+
 
 if (lightAmount > 104) {
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
 }
 
+
 if (lightAmount > 156) {
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
 }
+
 
 if (lightAmount > 208) {
     neopixelStrip.setPixelColor(4, neopixel.colors(NeoPixelColors.Red))
