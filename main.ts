7005c84ca8ce8b7609096159d9531383801ab036
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-03-23 13:28:50 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,9 @@
 * This program shows the brightness level of light
 */
 
-
 // variables
 let neopixelStrip: neopixel.Strip = null
 let lightAmount: number = 0
-
 
 // clean
 basic.clearScreen()
@@ -21,29 +19,23 @@ neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
-
 lightAmount = input.lightLevel()
-
 
 if (lightAmount <= 51) {
     neopixelStrip.clear()
 }
 
-
 if (lightAmount > 52) {
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
 }
-
 
 if (lightAmount > 104) {
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
 }
 
-
 if (lightAmount > 156) {
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
 }
-
 
 if (lightAmount > 208) {
     neopixelStrip.setPixelColor(4, neopixel.colors(NeoPixelColors.Red))
