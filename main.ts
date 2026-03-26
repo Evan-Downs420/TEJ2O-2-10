07fe49f8ba554389603354094efbb33a12e34220
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-03-26 16:17:35 -0400

    wasd

--- main.ts
+++ main.ts
@@ -5,6 +5,8 @@
 * This program shows the brightness level of light
 */
 
+let neopixelStrip: neopixel.Strip = null
+let lightAmount: number = 0
 // variables
 let neopixelStrip: neopixel.Strip = null
 let lightAmount: number = 0
@@ -19,22 +21,27 @@ neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
+input.onButtonPressed(Button.A, function() {
+
   lightAmount = input.lightLevel()
 
-input.onButtonPressed(Button.A, function(){
-    if (lightAmount <= 51) {
+  if (lightAmount <= 51) {
       neopixelStrip.clear()
-    }
-    if (lightAmount > 52) {
+  }
+
+  if (lightAmount > 52) {
       neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-    }
-    if (lightAmount > 104) {
+  }
+
+  if (lightAmount > 104) {
       neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
-    }
-    if (lightAmount > 156) {
+  }
+
+  if (lightAmount > 156) {
       neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
-    }
-    if (lightAmount > 208) {
+  }
+
+  if (lightAmount > 208) {
       neopixelStrip.setPixelColor(4, neopixel.colors(NeoPixelColors.Red))
-    }
+  }
 })
